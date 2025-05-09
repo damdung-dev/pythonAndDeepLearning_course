9ec489800f2f8664c8b7b01b064f8640401307e3
damdung-dev <damducdung2001@gmail.com>
2025-05-09 20:12:08 +0700

    Update Students.py

--- assignment-20250404T085550Z-001/assignment/poo21_upgrade1/Students.py
+++ assignment-20250404T085550Z-001/assignment/poo21_upgrade1/Students.py
@@ -1,41 +1,44 @@
-class Student:
-  # Attributes: ID, name, semester, course
-  count = 0
-  # Constructor:
-  def __init__(self, name, semester, course):
-    self.__name     = name
-    self.__semester = semester
-    self.__course   = course
-    Student.count+= 1
-    self.__ID       = Student.count
-  # Getters/ setters
-  @property
-  def ID(self):
-    return self.__ID
-  @ID.setter
-  def ID(self, ID):
-    self.__ID = ID
-  @property
-  def name(self):
-    return self.__name
-  @name.setter
-  def name(self, name):
-    self.__name = name
-  @property
-  def semester(self):
-    return self.__semester
-  @semester.setter
-  def semester(self, semester):
-    self.__semester = semester
-  @property
-  def course(self):
-    return self.__course
-  @course.setter
-  def course(self, course):
-    self.__course = course
-  # Methods:
-  def __str__(self):
-    st=""
-    st+=f"-"*40
-    st+=f"\n{self.__ID:10}|{self.__name:10}|{self.__semester:10}|{self.__course}"
-    return st
+class Students:
+    #Attributes:
+    list_studentName = []
+    list_id = []
+    list_semester = []
+    listcourseName_input = []
+    count=0
+    #Constructor:
+    def __init__(self, ID, studentName, Semester, courseName):
+        self.__student_name=studentName
+        self.__semester=Semester
+        self.__course_name=courseName
+        Students.count += 1
+        self.__id = Students.count
+    @property
+    def id(self):
+        return self.__id
+    @id.setter
+    def id(self, ID):
+        self.__id = ID
+
+    @property
+    def student_name(self):
+        return self.__student_name
+
+    @student_name.setter
+    def student_name(self, studentName):
+        self.__student_name = studentName
+
+    @property
+    def semester(self):
+        return self.__semester
+
+    @semester.setter
+    def semester(self, Semester):
+        self.__semester = Semester
+
+    @property
+    def course_name(self):
+        return self.__course_name
+
+    @course_name.setter
+    def course_name(self, courseName):
+        self.__course_name = courseName
